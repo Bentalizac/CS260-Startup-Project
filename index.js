83a1d5884f0fca15b4985c4e5247019d693319fe
Eli Ellsworth <bentalizac@gmail.com>
2023-04-17 20:49:16 -0600

    Express Middleware

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
+
+// The service port may be set on the command line
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.static('public'));
